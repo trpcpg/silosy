da81284aec2641b0cfe6f3cb37c24244a003db70
trpcpg <tomasz.bramowski@gmial.com>
2021-08-17 11:47:49 +0200

    początek projektu

---
+++ src/test/java/net/trpcp/silosy/SilosyApplicationTests.java
@@ -0,0 +1,13 @@
+package net.trpcp.silosy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilosyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

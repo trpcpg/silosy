11be6ad5b10b42c4881d28d25923a43bd94c8ab9
trpcpg <tomasz.bramowski@gmial.com>
2021-08-17 11:46:49 +0200

    początek projektu

---
+++ src/main/java/net/trpcp/silosy/SilosyApplication.java
@@ -0,0 +1,13 @@
+package net.trpcp.silosy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilosyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilosyApplication.class, args);
+	}
+
+}
